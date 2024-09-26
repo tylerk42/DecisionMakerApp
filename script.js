1bd595e936d5058d91e4dd16f7ad863147c5dd7f
tylerk42 <tkerrigan28@students.stab.org>
2024-09-26 10:35:59 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,135 @@
-var outputString = "OMG soooo cute!";
-//this will set the content of the html element with the id output equal to the value of my variable called output string//
-document.getElementById("output").innerHTML = outputString;
-//add code here to create a new variable that will store the number of likes
-var numlikes=0;
-//display the value of numlikes on screen//
-document.getElementById("likes").innerHTML= numlikes;
-
-function getText() {
-  //saves what the user types into the text box to the variable comment
-  var comment = document.getElementById("log").value;
-  //add code here to display the new comment on the screen
-
-// prints the value of comment to console
-console.log(comment)
-// updates the value of outputString to include the new comment and a line break
-outputString= outputString + "<br>"+comment;
-//show the new value of output string on the screen
-document.getElementById("output").innerHTML=outputString;
+function showPrice(){
+    document.getElementById("sliderVal").innerHTML =  document.getElementById("Price").value
 }
 
-
-function increaseLikes(){
-  //add code here to increase the number of likes and show the updated value on the screen
-numlikes=numlikes+1;
-console.log(numlikes);
-//show the updated value on the screen
-document.getElementById("likes").innerHTML= numlikes;
+function showShoe(){
   
+    document.getElementById("feedback").innerHTML =  findShoe(document.getElementById('Brand').value, document.getElementById('Type').value, document.getElementById('Price').value)
+    console.log("test")
 }
 
-function decreaseLikes(){
-  //add code here to decrease the number of likes and show the updated value on the screen
-numlikes=numlikes-1;
-console.log(numlikes);
-document.getElementById("likes").innerHTML= numlikes;
-  
-}
+
+function findShoe(brand,type,price){
+    var feedback= "";
+    if (price<=120&&brand=="Nike"&&type=="Basketball"){
+        document.getElementById("image").src="Ja 1s.jpg";
+        feedback="JA 1";
+    }
+    else if (price>=120&&brand=="Nike"&&type=="Basketball"){
+        document.getElementById("image").src="Pg 6s.jpeg"
+        feedback="PG 6";
+    }
+    else if (price<=120&&brand=="Nike"&&type=="Cleats"){
+        document.getElementById("image").src="Nike Mercurial Superfly 9s.png"
+        feedback="Mercuiral Superfly 9 Academy";
+    }
+    else if (price>=120&&brand=="Nike"&&type=="Cleats"){
+        document.getElementById("image").src="Mercurial Superfly 10 Elites.jpeg"
+        feedback="Mercuiral Superfly 10 Elite";
+    }
+    else if (price<=120&&brand=="Nike"&&type=="Running"){
+        document.getElementById("image").src="Juniper Trail 3s.jpeg"
+        feedback="Juniper Trail 3";
+    }
+    else if (price>=120&&brand=="Nike"&&type=="Running"){
+        document.getElementById("image").src="Vaporfly 3s.jpeg"
+        feedback="Vaporfly 3";
+    }
+    else if (price<=120&&brand=="Nike"&&type=="Style"){
+        document.getElementById("image").src="Nike College Off Courts.jpeg"
+        feedback="College Off Court";
+    }
+    else if (price>=120&&brand=="Nike"&&type=="Style"){
+        document.getElementById("image").src="Vomero 5s.jpeg"
+        feedback="Vomero 5";
+    }
+    else if (price<=120&&brand=="Nike"&&type=="Comfort"){
+        document.getElementById("image").src="Pandas(Low Dunks).jpeg"
+        feedback="Dunk Low";
+    }
+    else if (price>=120&&brand=="Nike"&&type=="Comfort"){
+        document.getElementById("image").src="wetcement.jpg"
+        feedback="Wet Cement 4";
+    } 
+    else if (price<=120&&brand=="Adidas"&&type=="Basketball"){
+        document.getElementById("image").src="Harden 8s.jpeg"
+        feedback="Harden 8";
+    }
+    else if (price>=120&&brand=="Adidas"&&type=="Basketball"){
+        document.getElementById("image").src="don6.avif"
+        feedback="Don 6";
+    }
+    else if (price<=120&&brand=="Adidas"&&type=="Cleats"){
+        document.getElementById("image").src="adizero eletric.1s.jpeg"
+        feedback="Adizero Eletric 1";
+    }
+    else if (price>=120&&brand=="Adidas"&&type=="Cleats"){
+        document.getElementById("image").src="adiizero eletric+s.jpeg"
+        feedback="Adizero Eletric +";
+    }
+    else if (price<=120&&brand=="Adidas"&&type=="Running"){
+        document.getElementById("image").src="supernova strides.jpeg"
+        feedback="Supernova Stirde";
+    }
+    else if (price>=120&&brand=="Adidas"&&type=="Running"){
+        document.getElementById("image").src="ultraboost 1.0s.jpeg"
+        feedback="Ultra Boost 1.0";
+    }
+    else if (price<=120&&brand=="Adidas"&&type=="Style"){
+        document.getElementById("image").src="superstar shoes.jpeg"
+        feedback="Superstar Shoes";
+    }
+    else if (price>=120&&brand=="Adidas"&&type=="Style"){
+        document.getElementById("image").src="ultraboost 1.0s.jpeg"
+        feedback="Ultra Boost 1.0";
+    }
+    else if (price<=120&&brand=="Adidas"&&type=="Comfort"){
+        document.getElementById("image").src="adilette slides.jpeg"
+        feedback="Adillete Slides";
+    }
+    else if (price>=120&&brand=="Adidas"&&type=="Comfort"){
+        document.getElementById("image").src="ultraboost 1.0s.jpeg"
+        feedback="Ultra Boost 1.0"
+    }
+    else if (price>=120&&brand=="Under Armour"&&type=="Basketball"){
+        document.getElementById("image").src="Curry Flow 10s.jpeg"
+        feedback="Curry Flow 10";
+    }
+    else if (price<=120&&brand=="Under Armour"&&type=="Basketball"){
+        document.getElementById("image").src="Curry Splash 24s.jpeg"
+        feedback="Curry Splash 24";
+    }
+    else if (price>=120&&brand=="Under Armour"&&type=="Cleats"){
+        document.getElementById("image").src="ua blurs.jpeg"
+        feedback="Blur 2";
+    }
+    else if (price<=120&&brand=="Under Armour"&&type=="Cleats"){
+        document.getElementById("image").src="ua hihglight 2s.jpeg"
+        feedback="Highlight 2";
+    }
+    else if (price<=120&&brand=="Under Armour"&&type=="Running"){
+        document.getElementById("image").src="surge 4s.jpeg"
+        feedback="Surge 4";
+    }
+    else if (price>=120&&brand=="Under Armour"&&type=="Running"){
+        document.getElementById("image").src="ua velociti elite 2s.jpeg"
+        feedback="Velociti Elite 2";
+    }
+    else if (price<=120&&brand=="Under Armour"&&type=="Style"){
+        document.getElementById("image").src="curry foamtro slides.jpeg"
+        feedback="Curry Foamtro Slides";
+    }
+    else if (price>=120&&brand=="Under Armour"&&type=="Style"){
+        document.getElementById("image").src="ua slipspeed mega shoes.jpeg"
+        feedback="Slipspeed Mega";
+    }
+    else if (brand=="Under Armour"&&type=="Comfort"){
+        document.getElementById("image").src="surge 4s.jpeg"
+        feedback="Surge 4";
+    } 
+    return feedback
+    }
+    
+
+
+   
